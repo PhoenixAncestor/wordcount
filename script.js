be3e69386f94535fe16911cfe38e8877ee0f07e5
Phoenix <phoenixancestor@gmail.com>
2026-06-26 19:04:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-    const input = document.getElementById("input");
-    const info = document.getElementById("info");
-    const infoone = document.getElementById("infoone");
-    const aul = document.getElementById("autoupdatelabel");
-    const wordcount = document.getElementById("wordcount");
-    const submit = document.getElementById("count");
-    const check = document.getElementById("autoupdate");
-    const countPm = document.getElementById("countpunctuationmarks");
-    let cpm = false;
-    submit.addEventListener("click", countword);
-    function countPM() {
-      if(countPm.checked) {
-        cpm = true;
-      } else {
-        cpm = false;
-      }
-    }
-    function checktext() {
-      if(check.checked) {
+const input = document.getElementById("input");
+const info = document.getElementById("info");
+const infoone = document.getElementById("infoone");
+const aul = document.getElementById("autoupdatelabel");
+const wordcount = document.getElementById("wordcount");
+const submit = document.getElementById("count");
+const check = document.getElementById("autoupdate");
+const countPm = document.getElementById("countpunctuationmarks");
+const close = document.getElementById("close");
+let cpm = false;
+submit.addEventListener("click", countword);
+countPm.addEventListener("change", () => {
+    cpm = countPm.checked;
+});
+function checktext() {
+    if (check.checked) {
         countword();
-      }
     }
-    function countword() {
-      let original = input.value;
-      let final = 0;
-      if(cpm == true) {
+}
+function countword() {
+    let original = input.value;
+    let final = 0;
+    if (cpm) {
         let text = original.match(/[a-zA-Z0-9]+(?:['-][a-zA-Z0-9]+)*|[\u4e00-\u9fff]|-+|[^\sA-Za-z0-9_\u4e00-\u9fff-]/g) || [];
         final = text.length;
-      } else {
+    } else {
         let text = original.match(/[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*|[\u4e00-\u9fff]/g) || [];
         final = text.length;
-      }
-      wordcount.innerHTML = "Total of words: " + final;
     }
-    aul.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-      (info.classList.contains("show")) ? hide() : show();
-    });
-    info.addEventListener("click", (e) => {
-      if(!infoone.contains(e.target)) { hide(); }
-    });
-    function show() {
-      info.classList.add("show");
+    wordcount.textContent = "Total words: " + final;
+}
+aul.addEventListener("contextmenu", e => {
+    e.preventDefault();
+    info.classList.toggle("show");
+});
+close.addEventListener("click", () => {
+    info.classList.remove("show");
+});
+info.addEventListener("click", e => {
+    if (!infoone.contains(e.target)) {
+        info.classList.remove("show");
     }
-    function hide() {
-      info.classList.remove("show");
-    }
+});
